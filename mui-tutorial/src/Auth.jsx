64697932423f003d1a2eaa2b4173ca9743f531fd
Divya R <kwrdivyaraj100@gmail.com>
2023-12-06 17:14:31 +0530

    Created Auth component with form and styled Box

--- mui-tutorial/src/Auth.jsx
+++ mui-tutorial/src/Auth.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { Box, Button, TextField, Typography } from '@mui/material'
 
 function Auth() {
     return (
-        <div>Auth</div>
+        <div>
+            <form>
+                <Box display="flex" flexDirection={"column"} maxWidth={400} alignItems="center" justifyContent={"center"} margin="auto" marginTop={5} padding={3} borderRadius={5} boxShadow={'5px 5px 10px #ccc'} sx={{
+                    ":hover": {
+                        boxShadow: '10px 10px 20px #ccc',
+                    }
+                }}>
+                    <Typography>Login</Typography>
+                    <TextField />
+                    <TextField />
+                    <TextField />
+                    <Button>Login</Button>
+
+                </Box>
+            </form>
+        </div>
     )
 }
 
