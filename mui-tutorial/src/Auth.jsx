418f65785d0d7dcd6d9b42cb458b14fc3a57763a
Divya R <kwrdivyaraj100@gmail.com>
2023-12-06 17:35:43 +0530

    Updated Auth component to include signup form and state

--- mui-tutorial/src/Auth.jsx
+++ mui-tutorial/src/Auth.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Button, TextField, Typography } from '@mui/material'
 
 function Auth() {
+    const [isSignup, setIsSignup] = useState(false);
+    console.log(isSignup);
     return (
         <div>
             <form>
@@ -10,11 +12,13 @@ function Auth() {
                         boxShadow: '10px 10px 20px #ccc',
                     }
                 }}>
-                    <Typography variant='h2' padding={3} textAlign="center">Login</Typography>
-                    <TextField margin='normal' type={'text'} variant='outlined' placeholder='Name' />
-                    <TextField margin='normal' type={'email'}  variant='outlined' placeholder='Email' />
+                    <Typography variant='h2' padding={3} textAlign="center">{isSignup ? "Signup" : "Login"}</Typography>
+                    {isSignup && (<TextField margin='normal' type={'text'} variant='outlined' placeholder='Name' />
+                    )}
+                    <TextField margin='normal' type={'email'} variant='outlined' placeholder='Email' />
                     <TextField margin='normal' type={'password'} variant='outlined' placeholder='Password' />
-                    <Button sx={{marginTop: 3, borderRadius: 3 }} variant='contained' color='warning'>Login</Button>
+                    <Button sx={{ marginTop: 3, borderRadius: 3 }} variant='contained' color='warning'>Login</Button>
+                    <Button onClick={() => setIsSignup(!isSignup)} sx={{ marginTop: 3, borderRadius: 3 }} >Change to {isSignup ? "Login" : "signup"}</Button>
 
                 </Box>
             </form>
